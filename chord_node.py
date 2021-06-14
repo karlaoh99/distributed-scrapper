f8f8f61608888c1f7c8baf70297961c9a2b86b69
karlaoh99 <karlaolivera99@gmail.com>
2021-06-14 11:12:20 -0400

    Fix some comments and variables names

--- chord_node.py
+++ chord_node.py
@@ -18,29 +18,29 @@ class Node:
     
     @property
     def successor(self):
-        return get_node_instance(self.ft_node[1])
+        return get_node_instance(self._ft_node[1])
 
     @successor.setter
     def successor(self, new_id):
-        self.ft_node[1] = new_id
-        self.successor_list = []
+        self._ft_node[1] = new_id
+        self._successors_list = []
 
     @property
     def predecessor(self):
-        return get_node_instance(self.ft_node[0])
+        return get_node_instance(self._ft_node[0])
     
     @predecessor.setter
     def predecessor(self, new_id):
-        self.ft_node[0] = new_id
+        self._ft_node[0] = new_id
         self._predecessor_keys = self.predecessor.keys
     
     @property
-    def finger_table(self):
-        return [(self.ft_start[i], self.ft_node[i]) for i in range(1, self.m + 1)]
+    def ft_node(self):
+        return self._ft_node
 
     @property
-    def fingers(self):
-        return self.ft_node
+    def finger_table(self):
+        return [(self._ft_start[i], self._ft_node[i]) for i in range(1, self.m + 1)]
 
     @property
     def keys(self):
@@ -54,12 +54,14 @@ class Node:
     def predecessor_keys(self, new_keys):
         self._predecessor_keys = new_keys
     
-    def inrange(self, key, lwb, upb):                                         
+    def inrange(self, key, lwb, upb): 
+        '''
+        Return True if key is between lwb and upb in the 
+        ring of nodes, False in other case
+        '''                                        
         lwb = lwb % self.MAXPROC
         upb = upb % self.MAXPROC
-        if(key is None):
-            print(self.ft_node)
-            print(self.id)
+
         if lwb == upb:
             return True
         elif lwb < upb:                                                          
@@ -82,7 +84,7 @@ class Node:
         '''
         node = self
         temp = self
-        while not self.inrange(id, node.id + 1, node.fingers[1] + 1):
+        while not self.inrange(id, node.id + 1, node.ft_node[1] + 1):
             node = node.closest_preceding_finger(id)
             if node is None or node.id == temp.id:
                 break
@@ -94,38 +96,40 @@ class Node:
         Return the closest node finger preceding id
         '''
         for i in range(self.m, 0, -1):
-            if self.inrange(self.ft_node[i], self.id + 1, id):
-                node_id = self.ft_node[i]
+            if self.inrange(self._ft_node[i], self.id + 1, id):
+                node_id = self._ft_node[i]
                 return get_node_instance(node_id)
         return self
 
     def join(self, nodeid):
         '''
-        Return true if the node was succesfully joined to 
-        the system, false in other case
+        Return True if the node was successfully joined to 
+        the system, False in other case
         '''
-        print(nodeid)
         self._keys = {}
         self._predecessor_keys = {}
-        self.successor_list = []
+        self._successors_list = []
 
-        self.ft_start = [None] * (self.m + 1)
+        self._ft_start = [None] * (self.m + 1)
         for i in range(1, self.m + 1):
-            self.ft_start[i] = (self.id + pow(2, i-1)) % self.MAXPROC
+            self._ft_start[i] = (self.id + pow(2, i-1)) % self.MAXPROC
 
-        self.ft_node = [None] * (self.m + 1)
-        if nodeid is None:  # first node in the system
+        self._ft_node = [None] * (self.m + 1)
+        # Is the first node in the ring
+        if nodeid is None:  
             for i in range(0, self.m + 1):
-                self.ft_node[i] = self.id
+                self._ft_node[i] = self.id
             print(f'\nJoin of first node {self.id}')
-        else:  # is not the first node in the system  
+        # Is not the first node in the ring
+        else:    
             node = get_node_instance(nodeid)
-            # try:
-            self.init_finger_table(node)
-            self.update_others()
-            print(f'\nJoin node {self.id} with {nodeid}')
-            # except:
-            #     return False
+            try:
+                self.init_finger_table(node)
+                self.update_others()
+                print(f'\nJoin node {self.id} with {nodeid}')
+            except:
+                print(f'\nSomething went wrong trying to join node {self.id} with {nodeid}')
+                return False
         
         print_finger_table(self)
         return True
@@ -135,12 +139,12 @@ class Node:
         Initialize finger table of local node, starting
         from an arbitrary node already in the network
         '''
-        self.successor = node.find_successor(self.ft_start[1]).id
-        self.predecessor = self.successor.fingers[0]
+        self.successor = node.find_successor(self._ft_start[1]).id
+        self.predecessor = self.successor.ft_node[0]
 
-        keys_ = self.successor.keys.keys()
-        for key in keys_:
-            if self.inrange(key, self.fingers[0] + 1, self.id + 1):
+        succ_keys = self.successor.keys.keys()
+        for key in succ_keys:
+            if self.inrange(key, self.ft_node[0] + 1, self.id + 1):
                 self.keys[key] = self.successor.pop_key(key)  
         self.successor.successor.predecessor_keys = self.successor.keys
         
@@ -149,14 +153,14 @@ class Node:
         self.successor.predecessor = self.id
 
         for i in range(1, self.m):
-            if self.inrange(self.ft_start[i+1], self.id, self.ft_node[i]):
-                self.ft_node[i+1] = self.ft_node[i]
+            if self.inrange(self._ft_start[i+1], self.id, self._ft_node[i]):
+                self._ft_node[i+1] = self._ft_node[i]
             else:
-                self.ft_node[i+1] = node.find_successor(self.ft_start[i+1]).id 
+                self._ft_node[i+1] = node.find_successor(self._ft_start[i+1]).id 
                 
     def update_others(self):
         '''
-        Update all nodes whose finger tables should refer to self
+        Update all nodes whose finger tables should refer to local node
         '''
         for i in range(1, self.m + 1):
             p = self.find_predecessor((self.id - pow(2, i-1)) % self.MAXPROC)
@@ -165,41 +169,42 @@ class Node:
 
     def update_finger_table(self, s, i):
         '''
-        If s is ith finger of n, update self's finger table with s
+        If s is ith finger of local node, update local node's finger table with s
         '''
-        if self.inrange(s, self.id, self.ft_node[i]):
-            self.ft_node[i] = s
+        if self.inrange(s, self.id, self._ft_node[i]):
+            self._ft_node[i] = s
             p = self.predecessor
             if p is not None and p.id != s:
                 p.update_finger_table(s, i)
 
     def stabilize(self):
         '''
-        Periodically verify n’s immediate successor,
-        and tell the successor about n
+        Periodically verify local node’s immediate successor,
+        and tell the successor about local node
         '''
         while self.successor is None:
-            if not self.successor_list:
+            if not self._successors_list:
                 self.successor = self.id
                 return 
-            self.successor = self.successor_list.pop(0)  
+            self.successor = self._successors_list.pop(0)  
 
         x = self.successor.predecessor
-        if x is not None and self.inrange(x.id, self.id + 1, self.ft_node[1]) and ((self.id + 1) % self.MAXPROC) != self.ft_node[1]:
+        if x is not None and self.inrange(x.id, self.id + 1, self._ft_node[1]) and ((self.id + 1) % self.MAXPROC) != self._ft_node[1]:
             self.successor = x.id
         if self.successor is not None: 
             self.successor.notify(self)
 
     def notify(self, node):
         '''
-        Self thinks node might be our predecesor
+        Local node thinks node might be our predecesor
         '''
-        if self.predecessor is None or self.inrange(node.id, self.ft_node[0] + 1, self.id):
-            if self.predecessor is None:
-                for key in self._predecessor_keys.keys():
-                    self._keys[key] = self._predecessor_keys[key]
-                    if self.successor is not None:
-                        self.successor.update_key_in_succe(key, self._keys[key])
+        if self.predecessor is None:
+            for key in self._predecessor_keys.keys():
+                self._keys[key] = self._predecessor_keys[key]
+                if self.successor is not None:
+                    self.successor.update_predecessor_key(key, self._keys[key])
+    
+        if self.predecessor is None or self.inrange(node.id, self._ft_node[0] + 1, self.id):
             self.predecessor = node.id
 
     def fix_fingers(self):
@@ -207,62 +212,89 @@ class Node:
         Periodically refresh finger table entries
         '''
         i = random.randint(2, self.m)
-        succ = self.find_successor(self.ft_start[i])
-        if succ is not None:
-            self.ft_node[i] = succ.id
+        node = self.find_successor(self._ft_start[i])
+        if node is not None:
+            self._ft_node[i] = node.id
 
     def update_succesor_list(self):
         new_succ = None
-        if not self.successor_list:
+        if not self._successors_list:
             new_succ = self.successor
-        elif len(self.successor_list) <= self.m:
-            new_succ = self.successor_list[-1]
+        elif len(self._successors_list) <= self.m:
+            new_succ = self._successors_list[-1]
             new_succ = get_node_instance(new_succ)
         
         if new_succ is not None:
-            self.successor_list.append(new_succ.id)
+            self._successors_list.append(new_succ.id)
 
     def lookup(self, key):
+        '''
+        Return the node responsible for storing the key 
+        '''
         while True:
-            if self.inrange(key, self.ft_node[0] + 1, self.id + 1):
+            if self.inrange(key, self._ft_node[0] + 1, self.id + 1):
                 return self
             else:
                 for k in range(1, self.m):
-                    if self.inrange(key, self.ft_start[k], self.ft_start[k + 1]): 
-                        node = get_node_instance(self.ft_node[k])
+                    if self.inrange(key, self._ft_start[k], self._ft_start[k + 1]): 
+                        node = get_node_instance(self._ft_node[k])
                         if node is not None:       
                             return node.lookup(key)
                 else:
-                    node = get_node_instance(self.ft_node[self.m])
+                    node = get_node_instance(self._ft_node[self.m])
                     if node is not None:
                         return node.lookup(key)
 
-    def update_key(self, key, content):
-        self._keys[key] = content
+    def update_key(self, key, value):
+        '''
+        Update the value of a key in local node 
+        '''
+        self._keys[key] = value
 
     def pop_key(self, key):
-        return self._keys.pop(key)
+        '''
+        Delete a key in local node and returns its value
+        '''
+        if key in self._keys.keys():
+            value = self._keys.pop(key) 
+            print(f'Key {key} was deleted in node {self.id}')
+            return value
+        
+        print(f'Something went wrong trying to delete key {key} in node {self.id}')
+        return None 
     
-    def update_key_in_succe(self, key, content):
-        self._predecessor_keys[key] = content
+    def update_predecessor_key(self, key, value):
+        '''
+        Update the value of a key in predecessor_keys dictionary 
+        in local node
+        '''
+        self._predecessor_keys[key] = value
     
     def save_key(self, key, value):
         '''
-        Save a key and a value in the system 
+        Save a key and its value in the system and return 
+        True if the operation was successfully and False in
+        other case
         '''
         node = self.lookup(key)
         if node is not None:
             node.update_key(key, value)
-            node.successor.update_key_in_succe(key, value)
-            print(node.id)
+            node.successor.update_predecessor_key(key, value)
+            print(f'Key {key} was saved in node {node.id}')
+            return True
+        
+        print(f'Something went wrong trying to save key {key} in the system')
+        return False
 
     def get_value(self, key):
         '''
-        Return the value of a key
+        Return the value of a key stored in the system
         '''
         node = self.lookup(key)
         if node is not None and key in node.keys.keys():
             return node.keys[key]
+        
+        print(f'Something went wrong trying to get the value of key {key} in the system')
         return None        
 
 
@@ -288,8 +320,8 @@ def stabilize_function(node):
 def print_finger_table(node):
     if node is not None:
         print(f'Node {node.id}')
-        print(f'Predecessor: {node.fingers[0]}')
-        print(f'Successor: {node.fingers[1]}')
+        print(f'Predecessor: {node.ft_node[0]}')
+        print(f'Successor: {node.ft_node[1]}')
         for i in node.finger_table:
             print(f'Start: {i[0]}   Node: {i[1]}')
         print(node.keys)
